a3993774e1455dca519bdc70feada9cc2ec83701
blim0028 <128458112+blim0028@users.noreply.github.com>
2025-10-19 03:49:21 +0800

    Chroma DB and main app

--- embeddings.py
+++ embeddings.py
@@ -1,29 +1,52 @@
 import os
+from dotenv import load_dotenv
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAI, OpenAIEmbeddings
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.docstore.document import Document
+from langchain.chains.summarize import load_summarize_chain
 
 # File paths
 DATA_PATH = "data"
 CHROMA_DB_PATH = "chroma_db"
 
-documents = []
-for file in os.listdir(DATA_PATH):
-    if file.endswith('.txt'):
-        file_path = os.path.join(DATA_PATH, file)
-        with open(file=file_path, mode='r', encoding='utf-8') as x:
-            transcript = x.read()
-        documents.append(Document(page_content=transcript, metadata={"source":file}))
+load_dotenv()
+print("API Key loaded:", os.getenv("OPENAI_API_KEY") is not None)
 
-print(f"Loaded {len(documents)} meeting transcripts")
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+    temperature=0
+)
+summarize_chain = load_summarize_chain(llm, chain_type="map_reduce")
 
 text_splitter = RecursiveCharacterTextSplitter(
     separators=['\n\n', '\n'],
-    chunk_size = 1000,
+    chunk_size = 1200,
     chunk_overlap = 200
 )
 
-chunks = text_splitter.split_documents(documents=documents)
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 
-print(f"The length of chunks is {len(chunks)}")
+all_summaries = []
+for file in os.listdir(DATA_PATH):
+    if file.endswith('.txt'):
+        file_path = os.path.join(DATA_PATH, file)
+        with open(file=file_path, mode='r', encoding='utf-8') as f:
+            transcript = f.read()
+        
+        doc = Document(page_content=transcript, metadata={"source": file})
+        summary = summarize_chain.invoke([doc])
+        print(f"✅ Summary complete for {file}")
+
+        summary_docs = text_splitter.create_documents(
+            [summary],
+            metadatas=[{'source': file}]
+        )
+        
+        all_summaries.extend(summary_docs)
+        
+print(f"\n✅ Total summarized chunks ready for embedding: {len(all_summaries)}")
+
+db = Chroma.from_documents(documents=all_summaries, embedding=embeddings, collection_name='meeting_summaries', persist_directory=CHROMA_DB_PATH)
+
+print(f"Chroma DB created and saved at '{CHROMA_DB_PATH}'")
--- main.py
+++ main.py
@@ -4,8 +4,17 @@ import streamlit as st
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAI, OpenAIEmbeddings
 from langchain.chains.summarize.chain import load_summarize_chain
+from langchain.agents import initialize_agent, Tool
+
+load_dotenv()
+
+CHROMA_DB_PATH = "chroma_db"
 
 # Main page
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+db = Chroma(persist_directory=CHROMA_DB_PATH, embedding_function=embeddings)
+retriever = db.as_retriever(search_kwargs={"k":3})
+
 st.title("Meeting Summarizer")
 
 query = st.text_input("Ask a question or set a task for past meetings")
@@ -14,3 +23,13 @@ response = st.button("Enter")
 
 if response and query:
     st.write("HIHI")
+    
+
+    # retriever = db.as_retriever(search_kwargs={"k": 3})
+
+    # llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    # qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
+    # answer = qa_chain.run(query)
+
+    # st.subheader("💡 Answer")
+    # st.write(answer)
